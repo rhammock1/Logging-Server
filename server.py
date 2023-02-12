1897747fc43aa407becabbf7163134feb88e25f0
rhammock1 <robert.hammock@gotab.io>
2023-02-12 18:51:50 -0500

    WIP POST request

--- server.py
+++ server.py
@@ -21,6 +21,18 @@ class LogServer(BaseHTTPRequestHandler):
     self._set_response()
     self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
+  def do_POST(self):
+    content_length = int(self.headers['Content-Length'])
+    post_data = self.rfile.read(content_length)
+    logging.info(
+      "POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+      str(self.path),
+      str(self.headers),
+      post_data.decode('utf-8')
+    )
+    self._set_response()
+    self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
 def run(server_class=HTTPServer, handler_class=LogServer, port=8000):
   logging.basicConfig(level=logging.INFO)
   server_address = ('', port)
