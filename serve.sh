6e33ba01374a98bd2fc07f95c4a7ddbec3ee9d86
rhammock1 <robert.hammock@gotab.io>
2023-02-13 00:46:11 -0500

    WIP watchdog during development

--- serve.sh
+++ serve.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-PORT=''
-if [ $1 ]; then
-  PORT=$1
-fi
+if [ "$1" = "--dev" ]; then
+  echo "Starting server in development mode..."
+  python3.9 server.py & 
 
-echo "Starting server..."
-python3.9 server.py $PORT
+  SERVER_PROCESS_ID=$!
+  echo "Server process ID: $SERVER_PROCESS_ID"
+
+  echo "Watching server for changes..."
+  echo "Press Ctrl+C to stop watching."
+  # Watch the server for changes
+  # while true; do
+  #   # Check if a file has been saved or created in this directory
+
+
+  #   echo "Restarting server..."
+  #   kill $SERVER_PROCESS_ID
+  #   python3.9 server.py &
+  #   SERVER_PROCESS_ID=$!
+else 
+  echo "Starting server..."
+  python3.9 server.py
+fi
