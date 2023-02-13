5c1ae4fae123af3748364a44c8c7a32c4c17075a
rhammock1 <robert.hammock@gotab.io>
2023-02-12 23:53:27 -0500

    database module

---
+++ db.py
@@ -0,0 +1,63 @@
+from dotenv import load_dotenv
+import logging
+import psycopg2
+import os
+
+load_dotenv()
+
+connection = None
+database = None
+
+def db_file(filepath, *args):
+  """
+  Reads a file and returns the contents as a string.
+  For now, these files should use the `%s` syntax for string
+  interpolation.
+  \n`Args`: 
+  \n\t filepath (str): The path to the file to read.
+  \n\t args (tuple): The parameters to interpolate into the file.
+  \n`Returns`: The contents of the file as a string.
+  """
+  try: 
+    file_contents = ""
+    with open(filepath, "r") as file:
+      file_contents = file.read()
+
+    (params,) = args
+
+    # Make sure file_contents isn't an empty string
+    if file_contents == "":
+      raise Exception("File is empty: %s" % filepath)
+  except Exception as error:
+    logging.error("Error while reading file: %s", filepath, error)
+  
+  try:
+    query = file_contents.format(*params)
+    database.execute(query)
+    connection.commit()
+  except (Exception, psycopg2.Error) as error:
+    logging.error("Error while executing query: %s", query, error)
+
+def connect_to_db():
+  try:
+    connection_string = "dbname=%s user=%s" % (
+        os.getenv("DATABASE_NAME"), os.getenv("DATABASE_USER"))
+
+    global connection
+    connection = psycopg2.connect(connection_string)
+
+    global database
+    database = connection.cursor()
+
+    logging.info("Connected to database...")
+  except (Exception, psycopg2.Error) as error:
+    logging.error("Error while connecting to database", error)
+
+def close_db_connection():
+  try:
+    if connection:
+      database.close()
+      connection.close()
+      logging.info("Database connection closed.")
+  except (Exception, psycopg2.Error) as error:
+    logging.error("Error while closing database connection", error)
--- server.py
+++ server.py
@@ -8,47 +8,9 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from dotenv import load_dotenv
 import logging
 import json
-import psycopg2
-import os
+from db import *
 
 load_dotenv()
-
-connection = None
-database = None
-
-def db_file(filepath, *args):
-  """
-  Reads a file and returns the contents as a string.
-  For now, these files should use the `%s` syntax for string
-  interpolation.
-  \n`Args`: 
-  \n\t filepath (str): The path to the file to read.
-  \n\t args (tuple): The parameters to interpolate into the file.
-  \n`Returns`: The contents of the file as a string.
-  """
-  file_contents = ""
-  with open(filepath, "r") as file:
-    file_contents = file.read()
-
-  (params,) = args
-
-  # Make sure file_contents isn't an empty string
-  if file_contents == "":
-    raise Exception("File is empty: %s" % filepath)
-
-  query = file_contents.format(*params)
-  database.execute(query)
-  connection.commit()
-
-def connect_to_db():
-  try:
-    connection_string = "dbname=%s user=%s" % (os.getenv("DATABASE_NAME"), os.getenv("DATABASE_USER"))
-    connection = psycopg2.connect(connection_string)
-    cursor = connection.cursor()
-    logging.info("Connected to database...")
-    return connection, cursor
-  except (Exception, psycopg2.Error) as error:
-    logging.error("Error while connecting to database", error)
 
 def save_message(message, project):
   db_file("db/messages/insert.sql", (message, project,))
@@ -90,8 +52,7 @@ def run(server_class=HTTPServer, handler_class=LogServer, port=8000):
   httpd = server_class(server_address, handler_class)
   try:
     logging.info("Connecting to database...")
-    global connection, database
-    connection, database = connect_to_db()
+    connect_to_db()
 
     logging.info("Starting HTTP server on port: %d\n", port)
     httpd.serve_forever()
@@ -100,8 +61,7 @@ def run(server_class=HTTPServer, handler_class=LogServer, port=8000):
   logging.info('Stopping HTTP server...')
   httpd.server_close()
   logging.info('Closing database connection...')
-  database.close()
-  connection.close()
+  close_db_connection()
 
 
 if __name__ == '__main__':
