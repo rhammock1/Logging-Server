b145719f39f398da3c0948c8c103631922c7ffaa
rhammock1 <robert.hammock@gotab.io>
2023-02-13 19:32:07 -0500

    Fix column types

--- db/db_migrate/1.sql
+++ db/db_migrate/1.sql
@@ -1,12 +1,12 @@
 CREATE TABLE messages (
-  message_id BIGINT PRIMARY KEY,
+  message_id BIGSERIAL PRIMARY KEY,
   message TEXT NOT NULL,
   project TEXT NOT NULL,
   created TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE projects (
-  project_id BIGINT PRIMARY KEY,
+  project_id BIGSERIAL PRIMARY KEY,
   project TEXT NOT NULL,
   created TIMESTAMPTZ DEFAULT NOW()
 );
