53e91e57f22b38af4d5149934b79d9173a933424
rhammock1 <robert.hammock@gotab.io>
2023-02-15 18:18:16 -0500

    WIP Enable usage of ssl cert

--- server.py
+++ server.py
@@ -7,27 +7,40 @@
 from http.server import HTTPServer
 from dotenv import load_dotenv
 import logging
+import os
+import ssl
 from db import *
 from LogServer import LogServer
 
 load_dotenv()
 
-def run(server_class=HTTPServer, handler_class=LogServer, port=8000):
+def run(server_class=HTTPServer, handler_class=LogServer, use_ssl=0):
   """
   Starts the server.
   """
   logging.basicConfig(level=logging.INFO)
+  port = int(os.getenv("PORT"))
   server_address = ('', port)
   httpd = server_class(server_address, handler_class)
+  if use_ssl:
+    print("Using SSL")
+    httpd.socket = ssl.wrap_socket(
+      httpd.socket,
+      keyfile=os.getenv("SSL_KEY_PATH"),
+      certfile=os.getenv("SSL_CERT_PATH"), server_side=True
+    )
+  protocol = 'HTTPS' if use_ssl else 'HTTP'
+  # TODO
+  # What happens if an HTTPS request is made to an HTTP server?
   try:
     logging.info("Connecting to database...")
     connect_to_db()
 
-    logging.info("Starting HTTP server on port: %d\n", port)
+    logging.info("Starting %s server on port: %d\n", protocol, port)
     httpd.serve_forever()
   except KeyboardInterrupt:
     pass
-  logging.info('Stopping HTTP server...')
+  logging.info("Stopping %s server...", protocol)
   httpd.server_close()
   logging.info('Closing database connection...')
   close_db_connection()
@@ -36,7 +49,8 @@ def run(server_class=HTTPServer, handler_class=LogServer, port=8000):
 if __name__ == '__main__':
   from sys import argv
 
-  if len(argv) == 2:
-    run(port=int(argv[1]))
+  if (len(argv) == 2
+      and (argv[1] == "--ssl" or argv[1] == "-s")):
+    run(use_ssl=1)
   else:
     run()
